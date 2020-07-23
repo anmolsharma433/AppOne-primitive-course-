aebe89dbe7d94b25e6998671b0706c5544489050
Anmol Sharma <anmolsharma433@gmail.com>
2020-07-23 11:59:19 -0400

    appose -primitive images

--- AppOne/ContentView.swift
+++ AppOne/ContentView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center) {
+            
+            Image("me")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:200)
+                .clipShape(Circle())
+                .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .shadow(radius: 30, x:10, y:10)
+           // .overlay(Circle().stroke(Color.white,lineWidth:4).shadow(radius: 10))
+                .overlay(Circle().stroke(Color.white,lineWidth: 11).shadow(radius: 15))
+            
+            Text("Hey My name is Anmol Sharma and i am leraning Swift UI.")
+                .kerning(4)
+                .font(.caption)
+                .frame(width: 300, height:100)
+                .background(Color.white)
+                //.cornerRadius(64)
+            .clipShape(Rectangle())
+                .padding(20)
+            
+                //.clipShape(RoundedRectangle(cornerRadius: 64,style: .continuous))
+                .shadow(radius: 30, x:10, y:30)
+
+            
+               
+            
+        }.frame(minWidth: 0, idealWidth: 300, maxWidth: .infinity, minHeight: .infinity, idealHeight: .infinity, maxHeight: .infinity, alignment: .top)
+        
     }
 }
 
